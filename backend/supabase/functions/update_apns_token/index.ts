52b2df554407bf705492e65f0c31ba0dbe7d1fe6
kevinislinger <kevin.islinger@ik.me>
2025-09-02 14:28:25 +0200

    🐛 fix: edge functions
    - use anon key

--- backend/supabase/functions/update_apns_token/index.ts
+++ backend/supabase/functions/update_apns_token/index.ts
@@ -12,7 +12,7 @@ serve(async (req) => {
     // Create Supabase client
     const supabaseClient = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
-      Deno.env.get('SUPABASE_PUBLISHABLE_API_KEY') ?? ''
+      Deno.env.get('SUPABASE_ANON_KEY') ?? ''
     );
 
     // Get the authorization header
